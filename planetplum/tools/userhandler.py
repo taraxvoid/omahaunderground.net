db5bb009d0e804e8667eb89687d6d0c6ae5103da
void <1711810+v01dpointer@users.noreply.github.com>
2026-05-19 10:17:20 -0500

    fix #37

--- planetplum/tools/userhandler.py
+++ planetplum/tools/userhandler.py
@@ -4,11 +4,17 @@ def ConfirmUser(user, orgType=None, organization=None):
     if user.is_admin(): 
         return True
     match orgType:
-        case "band" | "bandlink":
+        case "band":
             if user in organization.members.all(): return True
-            if organization.label: 
+            if organization.label:
                 if user in organization.label.associates.all(): return True
             if user.email == organization.email: return True
+        case "bandlink":
+            band = organization.band
+            if user in band.members.all(): return True
+            if band.label:
+                if user in band.label.associates.all(): return True
+            if user.email == band.email: return True
         case "label":
             if user in organization.associates.all(): 
                 return True
